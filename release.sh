f32b4ae3e2b1567d57e508fab89f9d56ebaab297
Mathew Munro <mathew.munro@infinityworks.com>
2018-02-28 16:01:23 +0000

    update

--- release.sh
+++ release.sh
@@ -43,7 +43,7 @@ fi
 ## Get app name and current version in package.json
 package_file='package.json'
 package=$(cat package.json)
-app_name=$(echo package | jq -r ."name")
-current_version=$(echo package | jq -r ."version")
+app_name=$(echo package | jq -r '."name"')
+current_version=$(echo package | jq -r '."version"')
 
 echo "--------------"
