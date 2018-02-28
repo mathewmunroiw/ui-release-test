b97cdd8bd65f0d462259ab809fb4e3e7f6011d50
Mathew Munro <mathew.munro@infinityworks.com>
2018-02-28 15:28:07 +0000

    more

--- release.sh
+++ release.sh
@@ -6,3 +6,34 @@ current_version=$(echo $parameters | jq -r .["\"$environment\""].common.paramete
 
 echo "Current Branch [$current_branch]"
 echo "Target Release Branch [$target_release_branch]"
+
+if [ -z $(git status --porcelain) ];
+then
+    echo "\xE2\x9C\x94 Nothing to Commit"
+else
+    echo "PLEASE COMMIT YOUR CHANGE FIRST!!!"
+    echo $(git status --porcelain)
+fi
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
+
+
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
